e5c95ca831d9f7701f0456042500e2e8e07daeba
Desvelados <DesveladosD@gmail.com>
2015-10-16 19:56:52 -0300

    MainPush

---
+++ Teclas/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Teclas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.f1lbl = new System.Windows.Forms.Label();
+            this.f2lbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "F1 Circulos";
+            // 
+            // f1lbl
+            // 
+            this.f1lbl.AutoSize = true;
+            this.f1lbl.Location = new System.Drawing.Point(85, 6);
+            this.f1lbl.Name = "f1lbl";
+            this.f1lbl.Size = new System.Drawing.Size(59, 13);
+            this.f1lbl.TabIndex = 2;
+            this.f1lbl.Text = "APAGADO";
+            // 
+            // f2lbl
+            // 
+            this.f2lbl.AutoSize = true;
+            this.f2lbl.Location = new System.Drawing.Point(85, 29);
+            this.f2lbl.Name = "f2lbl";
+            this.f2lbl.Size = new System.Drawing.Size(59, 13);
+            this.f2lbl.TabIndex = 4;
+            this.f2lbl.Text = "APAGADO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "F2 Spam z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(159, 88);
+            this.Controls.Add(this.f2lbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.f1lbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "PokeControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label f1lbl;
+        private System.Windows.Forms.Label f2lbl;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Teclas/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Teclas
+{
+    public partial class Form1 : Form
+    {
+        private KeyHandler F1;
+        private KeyHandler F2;
+        private bool corriendo,hablando = false;
+        private static System.Timers.Timer timerF1, timerF2;
+
+        public Form1()
+        {
+            F1 = new KeyHandler(Keys.F1, this);
+            F2 = new KeyHandler(Keys.F2, this);
+            F1.Register();
+            F2.Register();
+            InitializeComponent();
+
+            f1lbl.ForeColor = Color.Red;
+            f2lbl.ForeColor = Color.Red;
+
+            timerF1 = new System.Timers.Timer(2010);
+            timerF1.Elapsed += OnTimedEvent;
+            timerF1.AutoReset = true;
+
+            timerF2 = new System.Timers.Timer(700);
+            timerF2.Elapsed += OnTimedEvent2;
+            timerF2.AutoReset = true;
+        }
+        private void HandleF1()
+        {
+            if (corriendo)
+            {
+                timerF1.Stop();
+                corriendo = false;
+                f1lbl.ForeColor = Color.Red;
+                f1lbl.Text = "APAGADO";
+            }
+            else
+            {
+                timerF1.Start();
+                corriendo = true;
+                f1lbl.Text = "ENCENDIDO";
+                f1lbl.ForeColor = Color.Green;
+            }
+        }
+        private void HandleF2()
+        {
+            if (hablando)
+            {
+                timerF2.Stop();
+                hablando = false;
+                f2lbl.ForeColor = Color.Red;
+                f2lbl.Text = "APAGADO";
+            }
+            else
+            {
+                timerF2.Start();
+                hablando = true;
+                f2lbl.Text = "ENCENDIDO";
+                f2lbl.ForeColor = Color.Green;
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                if (m.WParam.ToInt32() == F1.GetId())
+                    HandleF1();
+                else if (m.WParam.ToInt32() == F2.GetId())
+                    HandleF2();
+            base.WndProc(ref m);
+        }
+
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            SendKeys.SendWait("{UP}");
+            SendKeys.SendWait("{UP}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.SendWait("{RIGHT}");
+            SendKeys.SendWait("{RIGHT}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.SendWait("{DOWN}");
+            SendKeys.SendWait("{DOWN}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.SendWait("{LEFT}");
+            SendKeys.SendWait("{LEFT}");
+            System.Threading.Thread.Sleep(500);
+        }
+
+        private static void OnTimedEvent2(Object source, ElapsedEventArgs e)
+        {
+            SendKeys.SendWait("z");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            corriendo = false;
+            timerF1.Stop();
+            timerF1.Close();
+            timerF1.Dispose();
+            this.Close();
+        }
+    }
+}
